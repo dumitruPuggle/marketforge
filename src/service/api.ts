4b2ec323d9f168d7af68def21d4d432fd663d93d
Dumitru Cucu <46863241+dumitruPuggle@users.noreply.github.com>
2022-05-16 22:21:45 +0300

    Checkpoint

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api/',
+    baseURL: 'http://192.168.1.5:5000/api/',
     headers: {
         'Content-Type': 'application/json'
     }
