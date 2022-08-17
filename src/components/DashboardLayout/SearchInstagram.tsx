5055fddfdacc9292a79742ab8a471b976c219f8d
Dumitru Cucu <46863241+dumitruPuggle@users.noreply.github.com>
2022-08-17 10:53:35 +0300

    checkpoint 17.08.2022

--- src/components/DashboardLayout/SearchInstagram.tsx
+++ src/components/DashboardLayout/SearchInstagram.tsx
@@ -4,6 +4,7 @@ import {
   InputGroup,
   MultistepDialog,
 } from "@blueprintjs/core";
+import { TextField } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useMediaQuery } from "react-responsive";
 import InstagramAPIBase from "../../service/InstagramAPI/InstagramAPIBase";
@@ -14,6 +15,7 @@ interface IInstagramSetup {
 
 export function InstagramSetup({ state }: IInstagramSetup) {
   const [dialog, setDialogVisible] = state;
+  const [username, setUsername] = useState<string>('');
   const isMobile = useMediaQuery({ maxWidth: 830 });
 
   if (isMobile && dialog) {
@@ -28,31 +30,43 @@ export function InstagramSetup({ state }: IInstagramSetup) {
       navigationPosition="left"
       onClose={() => setDialogVisible(false)}
     >
-      <DialogStep id="select" panel={<SearchInstagram />} title="Validate" />
+      <DialogStep id="select" panel={<SearchInstagram onChange={(value: string) => setUsername(value)}/>} title="Validate" />
+      <DialogStep id="select" panel={<ViewProfile username={username} />} title="View" />
     </MultistepDialog>
   );
 }
 
-function SearchInstagram() {
-	const [query, setQuery] = useState('')
-	const SearchInstagramInstance = new InstagramAPIBase()
-	useEffect(() => {
-		setTimeout(async () => {
-			const {data} = await SearchInstagramInstance.getProfileInfo(query)
-			console.log(data)
-		}, 500)
-	}, [query])
+function SearchInstagram({onChange}: {onChange: (a: string) => void;}) {
   return (
     <div className={Classes.DIALOG_BODY}>
-      <InputGroup
-        asyncControl={true}
-        large={true}
-        leftIcon="search"
-				onChange={({target}) => setQuery(target.value)}
-        placeholder="Search your account - @mycacasha"
-      />
+      <TextField onChange={(e) => onChange(e.target.value)} className="w-100" label="Instagram" color="primary" focused />
     </div>
   );
+}
+
+
+function ViewProfile({username}: {username: string}){
+  const [data, setData] = useState(null);
+
+  const getDataFromUsername = async (user: string) => {
+    const SearchInstagramInstance = new InstagramAPIBase()
+    return SearchInstagramInstance.getProfileInfo(user);
+  }
+
+  useEffect(() => {
+    getDataFromUsername(username).then(({data}) => {
+      setData(data)
+      console.log(data)
+    })
+    
+  }, [])
+  if (data) return (
+    <div className={Classes.DIALOG_BODY}>
+      <img src={data?.['hd_profile_pic_versions'][0]?.['url']} style={{width: 150, height: 150, borderRadius: '100%'}} />
+      <p>{data?.['full_name']}</p>
+    </div>
+  )
+  return <></>
 }
 
 export default InstagramSetup;
--- src/service/InstagramAPI/InstagramAPIBase.ts
+++ src/service/InstagramAPI/InstagramAPIBase.ts
@@ -2,22 +2,23 @@ import axios, { AxiosResponse } from "axios";
 
 class InstagramAPIBase {
   private BASE_URL;
-  private PATH;
-  private QUERY;
   private api;
   constructor() {
-    this.BASE_URL = "https://www.instagram.com";
-    this.PATH = "/web/search/topsearch";
-    this.QUERY = "?query=";
+    this.BASE_URL = "https://instagram39.p.rapidapi.com/";
 
     this.api = axios.create({
       baseURL: this.BASE_URL,
-      timeout: 1000
     });
   }
-  
-  public getProfileInfo(name: string): Promise<AxiosResponse>{
-    return this.api.get(`${this.PATH}/${this.QUERY}${name}`)
+
+  public getProfileInfo(name: string): Promise<AxiosResponse> {
+    return this.api.get(`getProfileByUsername`, {
+      params: { username: name },
+      headers: {
+        "X-RapidAPI-Key": "b58eae33c8msha3089bbae27fc26p1f7b02jsn6114081af613",
+        "X-RapidAPI-Host": "instagram39.p.rapidapi.com",
+      },
+    }).then((response) => response.data);
   }
 }
 
