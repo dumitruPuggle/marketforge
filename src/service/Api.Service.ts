be9e32996de02b0a55bef69671df188f5a10dc37
Dumitru Cucu <46863241+dumitruPuggle@users.noreply.github.com>
2022-09-20 17:09:30 +0300

    Radar mechanism

--- src/service/Api.Service.ts
+++ src/service/Api.Service.ts
@@ -2,14 +2,23 @@ import axios, { Axios } from "axios";
 
 class ApiService {
   public baseURL = process.env.REACT_APP_BASE_URL;
+  // public backupURL = process.env.REACT_APP_BACKUP_URL;
+  public backupURL = "http://0.0.0.0:8000";
   public api: Axios;
   constructor() {
     this.api = axios.create({
-      baseURL: this.baseURL,
+      baseURL: this.getApiURL(),
       headers: {
         "Content-Type": "application/json",
       },
     });
+  }
+  protected getApiURL(): string {
+    const useBackup = localStorage.getItem("useBackupApi") === "true";
+    if (useBackup && this.backupURL) {
+      return this.backupURL;
+    }
+    return this.baseURL ? this.baseURL : "";
   }
 }
 
