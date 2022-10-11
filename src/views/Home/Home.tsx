0b9cc0d2c6a1adcd543d522da5049fc60d08d9db
Dumitru Cucu <46863241+dumitruPuggle@users.noreply.github.com>
2022-10-11 18:36:44 +0300

    Beautified the home page

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,12 +1,15 @@
-import NavBar from "../../components/NavBar/NavBar";
+// import NavBar from "../../components/NavBar/NavBar";
 import BuildingImage from '../../assets/homepage/under_construction.jpeg';
+import './Home.css';
 
 function Home(){
     return(
-        <div>
-            <NavBar />
-            <img style={{borderRadius: "20px"}} className="w-50 mb-3" alt="" src={BuildingImage} />
-            <h6>Production v1</h6>
+        <div className="flex-column flex-align-center">
+            {/* <NavBar /> */}
+            <img style={{borderRadius: "20px"}} className="w-50 mb-3 mt-5" alt="" src={BuildingImage} />
+            <small>This product is under construction</small>
+            <small className='text-medium'>Want to join our dev team? Contacts: t.me/Andriannicolaev</small>
+            <img draggable={false} style={{width: 20}} className="mt-3" src="https://upload.wikimedia.org/wikipedia/commons/a/a3/Cc.logo.circle.svg" />
         </div>
     )
 }
